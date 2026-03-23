336225c7a4ac05a4c3f06dc0d46d9964254814dc
syylvrbullet <ddch4z64@gmail.com>
2026-03-22 21:38:58 -0400

    Update fehw.sh

--- .config/i3/fehw.sh
+++ .config/i3/fehw.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-feh --no-fehbg --bg-scale '/home/fox/Pictures/bg/starcrop.png' '/home/fox/Pictures/bg/starcrop.png' '/home/fox/Pictures/bg/kwlo66.png'
+feh --no-fehbg --bg-scale '~/Pictures/bg/starcrop.png' '~/Pictures/bg/starcrop.png' '~/Pictures/bg/kwlo66.png'
