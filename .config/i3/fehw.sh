2215d5a6cf65fd708613ae4c33e92f5cd5f11a09
syylvrbullet <ddch4z64@gmail.com>
2026-03-22 21:37:51 -0400

    Add files via upload

---
+++ .config/i3/fehw.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+feh --no-fehbg --bg-scale '/home/fox/Pictures/bg/starcrop.png' '/home/fox/Pictures/bg/starcrop.png' '/home/fox/Pictures/bg/kwlo66.png'
---
+++ .config/i3/monitorfallback.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+xrandr --output HDMI-A-2 --right-of DVI-D-0 && xrandr --output VGA-1-1 --mode 1024x768 --rate 60.00 --right-of HDMI-A-2 --brightness 1.6 && xrandr --output DVI-D-0 --primary
